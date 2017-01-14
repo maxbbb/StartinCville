cf7dbd647eee567551080155e09b85a56c3d18d9
Jordan Rohrlich <jrohrlich@gmail.com>
2017-01-14 13:29:49 -0500

    All done. Mobile friendly :)

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,15 +3,7 @@ import '../styling/navbar.css';
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 import smoothScroll from 'smoothscroll';
 var icon = require('../menu.png')
-
-// window.onclick = function(event) {
-//   if (!event.target.matches('.dropbtn')) {
-//       Navigation.collapseMenu()
-//   }
-// }
-
 class Navigation extends Component {
-
     constructor(props) {
         super(props);
         this.state = {
@@ -23,31 +15,25 @@ class Navigation extends Component {
         this.handleSelectResources = this.handleSelectResources.bind(this);
         this.handleScroll = this.handleScroll.bind(this);
         this.menuDisplay = this.menuDisplay.bind(this);
-        this.collapseMenu = this.collapseMenu.bind(this)
     }
-
     handleSelectStory(event) {
         event.preventDefault();
         smoothScroll(document.querySelector('#Story'));
     }
-
     handleSelectCompanies() {
         event.preventDefault();
         smoothScroll(document.querySelector('#Companies'));
     }
-
     handleSelectResources() {
         event.preventDefault();
         smoothScroll(document.querySelector('#Resources'));
     }
-
     handleScroll() {
         var doc = document.documentElement
         var scrollTop = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0)
         var storyPosition = document.getElementById('Story').offsetTop - 2;
         var companiesPosition = document.getElementById('Companies').offsetTop - 2;
         var resourcesPosition = document.getElementById('Resources').offsetTop - 2;
-
         if (scrollTop > storyPosition && scrollTop < companiesPosition) {
             this.setState({ key: 'Story' })
         }
@@ -58,61 +44,22 @@ class Navigation extends Component {
             this.setState({ key: 'Resources' })
         };
     }
-
-    menuDisplay() {
-        console.log(this.state.menuShow)
-        this.setState({ menuShow: (this.state.menuShow === false ? true : false) });
-        console.log(this.state.menuShow)
+    menuDisplay(bool, event) {
+      console.log("menu display called")
+      if (bool) {
+        this.setState({ menuShow: !this.state.menuShow });
+        event.stopPropagation();
+      } else if (!bool && this.state.menuShow) {
+        this.setState({ menuShow: false})
+      }
     }
-
-    // collapseMenu(event) {
-    //     if (this.state.clickicon === false) {
-    //         if (this.state.menuShow === true) {
-    //             this.setState({ menuShow: false })
-    //         }
-    //      console.log('hey')
-    //     }
-    // }
-
-
-    // componentDidMount() {
-    //     window.addEventListener('scroll', this.handleScroll);
-    //     window.addEventListener('click', this.collapseMenu, false);
-    //     document.getElementById('toggle').addEventListener("click", this.collapseMenu, false)
-    // }
-
-    collapseMenu(bool) {
-        
-            if (this.state.menuShow === true) {
-                this.setState({ menuShow: false })
-            }
-            if (bool === true) {
-                this.setState({menuShow: true})
-            }
-
-         console.log('hey')
-    }
-
     componentDidMount() {
         window.addEventListener('scroll', this.handleScroll);
-        // window.addEventListener('click', this.collapseMenu, false);
-        // document.getElementById('toggle').addEventListener("click", this.collapseMenu(true), false)
-    }
-
-    /*
-        render() {
-            return (
-                <div className="App">
-                    <Nav activeKey={this.state.key} onSelect={this.handleSelect}>
-                        <a href="#Player"><img src={require('../cville.jpg')} /></a>
-                        <NavItem eventKey={1} href="#Story"> Story </NavItem>
-                        <NavItem eventKey={2} href="#Companies"> Companies </NavItem>
-                        <NavItem eventKey={3} href="#Resources"> Resources </NavItem>
-                    </Nav>
-                </div>
-                    )
+        if (document.getElementById('toggle')) {
+            document.getElementById('toggle').addEventListener("click", (event) => this.menuDisplay(true, event), true)
+            window.addEventListener('click', this.menuDisplay.bind(null, false), false )
         }
-    */
+    }
     render() {
         if (window.innerWidth > 730) {
             return (
@@ -128,8 +75,8 @@ class Navigation extends Component {
             return (
                 <div className="mobileNav">
                     <a href="#Player"><img src={require('../cville.jpg')} /></a>
-                    <div id="toggle" className="dropdown">
-                        <a onClick={this.menuDisplay} className="dropbtn"><img src={icon} /></a>
+                    <div className="dropdown">
+                        <a id="toggle" className="dropbtn"><img src={icon} /></a>
                         <div id="myDropdown" className={this.state.menuShow === true ? "show dropdown-content" : "dropdown-content"}>
                             <a href="#Story" className={(this.state.key === "Story") ? "active" : ""}>Story</a>
                             <a href="#Companies" className={(this.state.key === "Companies") ? "active" : ""}>Companies</a>
@@ -141,32 +88,4 @@ class Navigation extends Component {
         }
     }
 }
-
 export default Navigation;
-
-/*
-<a href="#Player"><img src={require('../cville.jpg')} /></a>
-
-const ControlledTabs = React.createClass({
-                    getInitialState() {
-                return {
-                    key: 1
-    };
-  },
-
-  handleSelect(key) {
-                    alert('selected ' + key);
-                this.setState({key});
-  },
-
-  render() {
-    return (
-      <Tabs activeKey={this.state.key} onSelect={this.handleSelect} id="controlled-tab-example">
-                    <Tab eventKey={1} title="Tab 1">Tab 1 content</Tab>
-                    <Tab eventKey={2} title="Tab 2">Tab 2 content</Tab>
-                    <Tab eventKey={3} title="Tab 3" disabled>Tab 3 content</Tab>
-                </Tabs>
-                );
-  }
-});
-*/
--- src/components/player.js
+++ src/components/player.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import '../styling/player.css';
 
+var frameHeight = (window.innerWidth < 800 ? window.innerHeight/2 : "auto")
+
 class Player extends Component {
     render() {
         return (
             <div id="Player">
-                <iframe src="https://player.vimeo.com/video/163275571?autoplay=1&title=0&byline=0&portrait=0&&color=FF9100" width="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                <iframe src="https://player.vimeo.com/video/163275571?autoplay=1&title=0&byline=0&portrait=0&&color=FF9100" width="100%" height={frameHeight} frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
                 <div className="intro-text">
                     <h1> Welcome to Charlottesville!</h1>
                     <a href="#Story"><h4> Meet our city.</h4> </a>
